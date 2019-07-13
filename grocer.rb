6426c75af4ced2a7700c34357d612ff7ba3a05d9
Belle P <bellepx3@gmail.com>
2019-07-13 22:39:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ end
 
 def checkout(cart, coupons)
     cd_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+  cart_with_coupons_applied = apply_coupons(cd_cart, coupons)
   cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
 
   total = 0.0
